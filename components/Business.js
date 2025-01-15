049eacb1263054250a76430d1787723c372cd265
unknown <sietkod@tcd.ie>
2025-01-15 20:57:24 +0000

    refactored business component to use props object passed to it

--- components/Business.js
+++ components/Business.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import styles from '../styles/Business.module.css';
 
-export default function Business({image, name, address, city, state, zipCode, category, rating, reviewCount}) {
+export default function Business(props) {
     return (
-        <div className={styles.business} alt={name}>
-            <img src={image}/>
-            <header className={styles.name} >{name}</header>
+        <div className={styles.business} alt={props.name}>
+            <img src={props.image}/>
+            <header className={styles.name} >{props.name}</header>
             <div className={styles.content}>
                 <div className={styles.businessInfo}>
-                    <p className={styles.address} >{address}</p>
-                    <p className={styles.city}>{city}</p>
-                    <p className={styles.zipCode}>{state} {zipCode}</p>
+                    <p className={styles.address} >{props.address}</p>
+                    <p className={styles.city}>{props.city}</p>
+                    <p className={styles.zipCode}>{props.state} {props.zipCode}</p>
                 </div>
                 <div className={styles.businessDetails}>
-                    <p className={styles.category}>{category}</p>
-                    <p className={styles.rating}>{rating + " stars"}</p>
-                    <p className={styles.reviewCount}>{reviewCount + " reviews"}</p>
+                    <p className={styles.category}>{props.category}</p>
+                    <p className={styles.rating}>{props.rating + " stars"}</p>
+                    <p className={styles.reviewCount}>{props.reviewCount + " reviews"}</p>
                 </div>
             </div>
         </div>
